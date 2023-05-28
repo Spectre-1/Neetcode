88c88563279086fb4a579029d1def251f41e5ce2
Spectre <pgudijan@uci.edu>
2023-05-27 17:04:10 -0700

    Group Anagram

---
+++ Arrays & Hashing/Group Anagrams/Group_Anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map <string, vector<string>> map1;
+
+        for(int i =0; i < strs.size(); i++)
+        {
+            string str1 = strs[i];
+            sort(str1.begin(), str1.end());
+            map1[str1].push_back(strs[i]);
+        }
+
+        for(auto x: map1)
+        {
+            ans.push_back(x.second);
+        }
+
+        return ans;
+    }
+};
